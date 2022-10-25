2443d8bac75140e1dd262e88855631edf9d99175
Juan Manuel Acosta <jmab2k@gmail.com>
2022-10-25 21:05:20 +0200

    Use state añade registros

--- src/pages/country/index.jsx
+++ src/pages/country/index.jsx
@@ -44,27 +44,27 @@ function Country() {
       country: "France",
       short: "FR",
     },
-    {
-      id: 7,
-      country: "Marruecos",
-      short: "MC",
-    },
   ]);
 
   function clic(clave) {
     alert("pte de implementar la " + clave);
   }
 
-  const countryAdd = (id, country, short) => {
-    setCountries(countries.push({ id: id, country: country, short: short }));
+  const countryAdd = () => {
+    let id = Math.random().floor;
+    let country = prompt("Introduzca Country");
+    let short = prompt("introduzca short");
+    if (country !== "" && short !== "") {
+      setCountries([...countries, { id: id, country: country, short: short }]);
+    }
   };
 
   const CountryDetail = ({ country, short, id }) => {
     return (
-      <div className="countryContent" key={Math.random()}>
-        <div key={Math.random()}>{country} </div>
-        <div key={Math.random()}>{short}</div>
-        <div key={Math.random()}>
+      <div className="countryContent" key={id}>
+        <div>{country} </div>
+        <div>{short}</div>
+        <div>
           <button
             type="button"
             onClick={(e) => {
@@ -90,23 +90,23 @@ function Country() {
   };
 
   return (
-    <div key={Math.random()}>
+    <div>
       <div className="panel">
         <div id="countryHeader">
           <div>COUNTRY</div>
           <div>SHORT</div>
         </div>
         <div id="countryItems">
-          {countries.map(({ country, short, id }) => {
-            return <CountryDetail country={country} short={short} id={id} />;
-          })}
+          {countries.map(({ country, short, id }) => (
+            <CountryDetail country={country} short={short} id={id} key={id} />
+          ))}
         </div>
         <div id="countryFooter">
           <button
             id="buttonCreate"
             type="button"
             onClick={(e) => {
-              countryAdd(10, "China", "CH");
+              countryAdd();
             }}
           >
             <img src={createIconSvg} alt="Create"></img>
