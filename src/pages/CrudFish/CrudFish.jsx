310737407a9aeb47a406b5086425825ec6ee1fa8
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2022-10-24 23:15:21 -0600

    feat: adding delete fish function

--- src/pages/CrudFish/CrudFish.jsx
+++ src/pages/CrudFish/CrudFish.jsx
@@ -1,28 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DeleteIcon from '../../assets/icon/🦆 icon _delete_.png'
 import CreateIcon from '../../assets/icon/🦆 icon _create_.png'
 import ReadMoreIcon from '../../assets/icon/read-more.png'
 import './CrudFish.css'
 
-const mockData = [
-  {
-    name: 'name',
-    scienticName: 'ScienticName',
-    family: 'family'
-  },
-  {
-    name: 'name2',
-    scienticName: 'ScienticName2',
-    family: 'family2'
-  },
-  {
-    name: 'name3',
-    scienticName: 'ScienticName3',
-    family: 'family3'
-  },
-]
 
 const CrudFish = () => {
+  const [mockData, setMockData] = useState([
+    {
+      id: 1,
+      name: 'name',
+      scienticName: 'ScienticName',
+      family: 'family'
+    },
+    {
+      id: 2,
+      name: 'name2',
+      scienticName: 'ScienticName2',
+      family: 'family2'
+    },
+    {
+      id: 3,
+      name: 'name3',
+      scienticName: 'ScienticName3',
+      family: 'family3'
+    },
+  ])
+  const deleteFish = (id) => {
+    setMockData(mockData.filter(fish => fish.id !== id))
+  }
+
   return (
     <div className='crud-container'>
       <div className='crud-card'>
@@ -38,8 +45,8 @@ const CrudFish = () => {
 
           <tbody>
             {
-              mockData.map(({name, scienticName, family}) => 
-                <tr>
+              mockData.map(({id, name, scienticName, family}) => 
+                <tr key={id}>
                   <td>{name}</td>
                   <td>{scienticName}</td>
                   <td>{family}</td>
@@ -50,7 +57,10 @@ const CrudFish = () => {
                     <button className='options-button'>
                       <img src={CreateIcon} alt="create-icon" />
                     </button>
-                    <button className='options-button'>
+                    <button 
+                      onClick={() => {deleteFish(id)}}
+                      className='options-button'
+                    >
                       <img src={DeleteIcon} alt="delete-icon" />
                     </button>
                   </td>
